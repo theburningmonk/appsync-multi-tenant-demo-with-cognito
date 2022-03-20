e4b0e27c1cbaf15ffe2104f0b093b3279f22201d
theburningmonk <theburningmonk@gmail.com>
2022-03-20 13:24:14 +0100

    fix: missing custom: in claim name

--- functions/add-user.js
+++ functions/add-user.js
@@ -2,7 +2,7 @@ const { createUser, addUserToGroup } = require('../lib/users')
 
 module.exports.handler = async (event) => {
   const { firstName, lastName, email } = event.arguments.input
-  const tenantId = event.identity.claims['tenant_id']
+  const tenantId = event.identity.claims['custom:tenant_id']
 
   const username = await createUser(firstName, lastName, email, tenantId)
   await addUserToGroup(username, 'User')
